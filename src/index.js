3ff5dd4e903917817f359e367461f0c9d5638e5c
Vladislav Kostyuk <logos535@gmail.com>
2023-07-06 11:39:14 +0300

    added validation

--- src/index.js
+++ src/index.js
@@ -1,2 +1,6 @@
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+
+import app from './view/view.js';
+
+app();
--- src/view/view.js
+++ src/view/view.js
@@ -1,41 +1,100 @@
 import onChange from 'on-change';
 import * as yup from 'yup';
 
-const state = {
-  rssForm: {
-    valid: null,
-    errors: [],
+//const schema = yup.object().shape({
+//  url: yup.string().url('Введите корректный URL').required(),
+//});
+
+const errorMessages = {
+  network: {
+    error: 'Network Problems. Try again.',
   },
 };
 
-const schema = yup.object().shape({
-  url: yup.string().url('Введите корректный URL').required('URL обязателен'),
-});
+const validate = async (fields) => {
+  try {
+    await schema.validate(fields, { abortEarly: false });
+    return {};
+  } catch (e) {
+    const errors = {};
+    for (let error of e.inner) {
+      errors[error.path] = error;
+    }
+    return errors;
+  }
+};
 
-schema
-  .validate({ url: 'некорректный_url' })
-  .then(() => {
-    console.log('Валидация успешна');
-  })
-  .catch((error) => {
-    console.log(error.message);
+const app = () => {
+  const state = {
+    rssForm: {
+      state: 'filling',
+      urls: [],
+      data: {
+        url: '',
+      },
+    },
+  };
+
+  const createError = (element) => {
+    element = element.nextElementSibling;
+    const newDiv = document.createElement('p');
+    element.classList.add('is-invalid');
+    newDiv.classList.add(
+      'feedback',
+      'm-0',
+      'position-absolute',
+      'small',
+      'text-danger'
+    );
+    newDiv.textContent = error.errors;
+    return element.after(newDiv);
+  };
+
+  const validateForm = () => {
+    const form = document.querySelector('.rss-form');
+    const urlInput = document.getElementById('url-input');
+    const feedback = document.querySelector('.feedback');
+
+    const schema = yup.object().shape({
+      url: yup.string().required().url().notOneOf(state.rssForm.urls),
+    });
+
+    const formData = {
+      url: urlInput.value.trim(),
+    };
+
+    schema
+      .validate(formData, { abortEarly: false })
+      .then(() => {
+        // Валидация успешна
+        urlInput.classList.remove('is-invalid');
+        feedback.textContent = '';
+        state.rssForm.urls.push(formData.url);
+        state.rssForm.state = 'valid';
+        // Очистка формы и установка фокуса
+        form.reset();
+        urlInput.focus();
+      })
+      .catch((error) => {
+        // Ошибки валидации
+        state.rssForm.state = 'invalid';
+        urlInput.classList.add('is-invalid');
+        feedback.textContent = error.errors[0];
+      });
+  };
+
+  const checkInput = (element, validMessage) => {};
+
+  const form = document.querySelector('form');
+  const inputUrl = document.querySelector('.url-input');
+  const submitButton = document.querySelector('form > div > div > button');
+  const inputs = form.querySelector('.url-input');
+
+  form.addEventListener('submit', async (e) => {
+    e.preventDefault();
+
+    validateForm();
   });
+};
 
-schema
-  .validate({ url: 'https://example.com' })
-  .then(() => {
-    console.log('Валидация успешна');
-  })
-  .catch((error) => {
-    console.log(error.message);
-  });
-
-const watchedState = onChange(state, (path, value, previousValue) => {
-  alert('value changed!');
-  console.log(path);
-  // => 'ui.value'
-  console.log(value);
-  // => 'other value'
-  console.log(previousValue);
-  // => 'hello'
-});
+export default app;
